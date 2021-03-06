b07cb2ecd40b3346c81964e79f320b2faf60c666
Vitor Barbosa Fonteles <73004721+Vito-Barbossa-Fonteles@users.noreply.github.com>
2021-03-06 13:30:52 -0300

    Create Classe Produto_e_precos.java

---
+++ Classe Produto_e_precos.java
@@ -0,0 +1,25 @@
+package Classes;
+
+public class Produtos_e_Precos {
+	
+	private String nome;
+	private double preco;	
+	
+	public void setNome(String nome) {
+		this.nome=nome;		
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco=preco;		
+	}
+	
+	public String getNome() {
+		return this.nome;		
+		
+	}
+	public double getPreco() {
+		return this.preco;
+	}
+		
+
+}
