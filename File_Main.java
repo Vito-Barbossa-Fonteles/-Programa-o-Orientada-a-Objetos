5e2aaeda5fdedb703957810c79aa1885a33a8ade
Vitor Barbosa Fonteles <73004721+Vito-Barbossa-Fonteles@users.noreply.github.com>
2021-03-06 13:30:09 -0300

    Update File_Main.java

--- File_Main.java
+++ File_Main.java
@@ -1,31 +1,32 @@
+package Main_files;
 import javax.swing.JOptionPane;
-
-public class Exerc {
-	public static void main(String args[]) {
-		int tamanho_vetor,contador,contador_preco=0,contador_40=0;
-		tamanho_vetor=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de produtos que deseja adicionar : "));
-		double preco_prod[]= new double[tamanho_vetor],media=0;
-		String nome_prod[]=new String[tamanho_vetor], resultado="";
-		for(contador=0;contador<tamanho_vetor;contador++) {
-		     nome_prod[contador]=JOptionPane.showInputDialog("Digite o nome do produto : ");
-			 preco_prod[contador]=Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do produto : "));	
-			 if(preco_prod[contador]<40) {
-					contador_40++;
-			 }else if(preco_prod[contador]>40&&preco_prod[contador]<100){	
-				 resultado = resultado +""+nome_prod[contador]+"\n";
-				}else {
-					contador_preco++;
-					media+=preco_prod[contador];	
-				}	
-		}
-		 if(resultado.equals("")){
-			 resultado+="\n Nenhum produto entre 40 e 100 reais cadastrado.";
-		 }
-		 System.out.println("."+resultado+".");
-		 if(contador_preco!=0) {
-				media/=contador_preco;
-				}
-		 JOptionPane.showMessageDialog(null,"Preço dos produtos entre 40 e 100 reais :\n"+resultado+"\n produtos com preço menor que 40 = "+contador_40+"\n"
-		 		+ "media dos produtos maiores que 100: "+media);
-	}
+import Calculos.Calcular;
+import Classes.Produtos_e_Precos;
+public class Exercicio {
+public static void main (String args[]) {
+	Calcular calc=new Calcular();
+	int tamanho_vetor,contador;
+	tamanho_vetor=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de produtos que deseja adicionar : "));
+	double[] preco_prod= new double[tamanho_vetor];
+	String[] nome_prod=new String[tamanho_vetor];
+	Produtos_e_Precos[] prod=new Produtos_e_Precos[tamanho_vetor];
+	String resultado="Preço dos produtos entre 40 e 100 reais : \n";
+	for(contador=0;contador<tamanho_vetor;contador++) {
+		 prod[contador]=new Produtos_e_Precos();
+		 prod[contador].setNome(nome_prod[contador]=JOptionPane.showInputDialog("Digite o nome do produto : "));
+		 prod[contador].setPreco(preco_prod[contador]=Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do produto : ")));	 
+		 calc.Calcular_infeior_40(prod[contador].getPreco());
+		 calc.Media_maior_100(prod[contador].getPreco());
+		 calc.Calcular_Entre40_100(prod[contador].getPreco());
+		 if(calc.getSeparar()!=0) {
+			 resultado = resultado +""+prod[contador].getNome()+"\n";
+			 }		 
+		 }	
+	 calc.Media_100();
+	 if(resultado.equals(" ")) {
+		 resultado="Nenhum produto entre 40 e 100 reais cadastrado.";
+	 }
+	 JOptionPane.showMessageDialog(null,""+resultado+"\n produtos com preço menor que 40 = "+calc.getContador()+"\n"
+	 		+ "media dos produtos maiores que 100: "+calc.getMedia());
+	 }
 }
